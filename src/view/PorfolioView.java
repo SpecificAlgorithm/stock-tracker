165b6c9f4c475e6833906b026196f58736d34758
Justin Lewis <cpluspluspython@gmail.com>
2017-03-25 17:20:38 -0500

    Fixed alot of Omar's code.  Omar if you could solve the error that exists that would be helpful.  Currently I dont know the type of exprobj so I cannot complete this code.  Thanks.

--- src/view/PorfolioView.java
+++ src/view/PorfolioView.java
@@ -13,6 +13,9 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.border.EmptyBorder;
+
+import com.sun.javafx.fxml.expression.Expression;
+
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
@@ -30,18 +33,16 @@ public class PorfolioView implements IView {
 	
 	// sellStock function Variables
 	private JFrame frame = new JFrame();
-	private JPanel contentPane;
-	private JTextField NumOFStocks;
+	private JPanel contentPane = new JPanel();
+	private JTextField NumOFStocks = new JTextField();
 	private JButton btnSell;
 	private JButton btnCancel;
 	private JTextField Company;
-	private JTextField Price;
-	contentPane = new JPanel();
-	NumOFStocks = new JTextField();
+	private JTextField Price = null;
 	JButton btnSell11;
 	JButton btnCancel11;
 	JLabel lblIfYouAre;
-	JRadioButton rdbtnMultiple1;
+//	JRadioButton rdbtnMultiple1;
 	JLabel lblNumOfStocks;
 	//
 	//
@@ -60,9 +61,9 @@ public class PorfolioView implements IView {
 	//
 	//PorfolioController controller;
 	
-	public PorfolioView() {
-		this.controller = new PorfolioController();
-	}
+//	public PorfolioView() {
+//		this.controller = new PorfolioController();
+//	}
 	    
 	public void initalize() {
 		rdbtnMultiple1.addActionListener(new ActionListener() {
@@ -233,6 +234,236 @@ public class PorfolioView implements IView {
 		Price.setColumns(10);
 		Price.setBounds(390, 272, 307, 42);
 		contentPane.add(Price);
+		
+		
+		
+		
+		btnSell.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				Connection connection;
+				if (rdbtnMultiple.isSelected()) {
+					String Num_Pat = "^[0-9]{1,2}$"; // Pattern set for integers
+														// FIXME : fix it and
+														// make more than 1
+					String Input = NumOFStocks.getText();
+					
+					if (expobj.regexChecker(Num_Pat, Input)) {
+						int multiple = Integer.parseInt(Input); // convert to
+																// integer INPUT
+						// stop
+						try {
+
+							// get number of stocks of a specific company out of
+							// the DB
+							// Subtract and make sure the user have enough
+							// update the Number of stocks
+
+							String query = "SELECT COUNT(*) FROM OwnedStocks WHERE Price = ?"; // un--tested
+							PreparedStatement pst = connection.prepareStatement(query);
+							pst.setString(1, Price.getText());
+							int Number = pst.getUpdateCount();
+							System.out.println(Number);
+							ResultSet rs = pst.executeQuery();
+							int count = 0;
+							while (rs.next())
+								count++;
+							if (count >= multiple)
+								JOptionPane.showMessageDialog(null, "Selling Proccesed");
+							else
+								JOptionPane.showMessageDialog(null, "You Dont Have enough Stocks");
+
+							System.out.println(count);
+							rs.close();
+							pst.close();
+						} catch (Exception e1) {
+							JOptionPane.showMessageDialog(null, "Cant COUNT!!!");
+						}
+
+						// end of "if multiple is chosen."
+
+					} else
+						JOptionPane.showMessageDialog(null, "Please enter 2 to 99 for Number of Stocks"); // When
+																											// the
+																											// pattern
+																											// does
+																											// not
+																											// match
+
+				} else if (!rdbtnMultiple.isSelected()) {
+					// if multiple is not selected
+					// just subtract one from the NumOfStocks Column of the
+					// company specified
+					// OR delete the the row if one stock FOUND!
+					//
+					// Steps followed:
+					// 1 - type a selection query
+					// 2- Pull the number of the stocks for the company
+					// 3- Decide If you would update " more than one found CASE"
+					// or delete "just one found CASE"
+
+					//
+					//   
+
+					int NumOFStocks = 0;
+					try {
+						// This query is unTESTED
+						String query = "SELECT NumOfStocks FROM OwnedStocks WHERE Company = ? AND Price = ?"; // FIXME!!
+						PreparedStatement pst = connection.prepareStatement(query); // ERROR
+																					// because
+																					// the
+																					// SQL
+																					// not
+																					// Linked
+																					// to
+																					// this
+																					// class
+																					// yet
+						pst.setString(1, Company.getText());
+						pst.setString(2, Price.getText());
+
+						ResultSet rs = pst.executeQuery();
+						pst.execute();
+
+						NumOFStocks = rs.getInt("NumOfStocks");
+
+						int count = 0;
+						while (rs.next()) {
+
+							//  
+						}
+
+						rs.close();
+						pst.close();
+					} catch (Exception e1) {
+						JOptionPane.showMessageDialog(null, "Error !!!");
+					}
+
+					if (NumOFStocks > 1) { // This case means the user have more
+											// than one
+											// stock of the company so we would
+											// just decrement
+
+						try {
+
+							String query = "UPDATE OwnedStocks WHERE Company = ? SET NumOfStocks = NumOfStocks - 1";
+							PreparedStatement pst = connection.prepareStatement(query);
+							pst.setString(1, Company.getText());
+							pst.execute();
+							pst.close();
+
+						} catch (Exception e1) {
+							JOptionPane.showMessageDialog(null, "Cant Subtract one");
+						}
+
+					} else if (NumOFStocks == 1) { // This case means the user
+													// just have one stock so we
+													// would delete the row
+
+						try {
+
+							String query = "DELETE FROM OwnedStocks WHERE Company = ? AND BoughtPrice = ?AND ";
+							// first check if subtract
+							PreparedStatement pst = connection.prepareStatement(query);
+
+							pst.setString(1, Company.getText());
+							pst.setString(2, Price.getText());
+
+							pst.execute();
+							pst.close();
+
+						} catch (Exception e1) {
+
+							JOptionPane.showMessageDialog(null, "CANT DELETE !!!");
+						}
+
+					} else { // This case runs when the user us trying to sell
+								// stocks he d
+
+						JOptionPane.showMessageDialog(null, "Company not found !!!");
+					}
+
+				}
+
+			}
+		});
+
+		// when cancel button pressed
+		btnCancel1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				contentPane.setVisible(false);
+				// find buy sell FIXME !!!!!!! FIX ME
+			}
+		});
+
+		// when buying multiple
+		rdbtnMultiple.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (rdbtnMultiple.isSelected()) { // the condition
+					lblNumOfStocks.setVisible(true);
+					NumOFStocks.setVisible(true);
+				} else if (!rdbtnMultiple.isSelected()) {
+					lblNumOfStocks.setVisible(false);
+					NumOFStocks.setVisible(false);
+				}
+
+			}
+		});
+
+		// Buying Stocks
+		// Move to Handlers
+		btnSell.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Connection connection;
+				Expression<T> expobj;
+				if (rdbtnMultiple.isSelected()) {
+					String Num_Pat = "^[0-9]{1,2}$"; // Reg exp pattern
+					String Input = NumOFStocks.getText(); // getting User input
+					int multiple = Integer.parseInt(Input); // change the input
+															// to int
+					if (expobj.regexChecker(Num_Pat, Input)) { // check if the
+																// pattern is
+																// right
+						JOptionPane.showMessageDialog(null, "Match"); // send a
+																		// message
+																		// that
+																		// the
+																		// pattern
+																		// matches
+						try {
+							String query = "INSERT INTO OwnedStocks (Company,BoughtPrice,NumOfStocks) VALUES (?,?,?)";
+							PreparedStatement pst = connection.prepareStatement(query);
+							pst.setString(1, Company.getText());
+							pst.setString(2, Price.getText());
+							pst.setInt(3, multiple);
+							pst.execute();
+							pst.close();
+
+						} catch (Exception e1) {
+							JOptionPane.showMessageDialog(null, "be Patient !!!");
+						}
+					} else
+						JOptionPane.showMessageDialog(null, "Input should be 2-99");
+
+				} else {
+					JOptionPane.showMessageDialog(null, "Thank god");
+					try {
+						String query = "INSERT INTO OwnedStocks (Company,BoughtPrice,NumOfStocks) VALUES (?,?,?)";
+						PreparedStatement pst = connection.prepareStatement(query);
+						pst.setString(1, Company.getText());
+						pst.setString(2, Price.getText());
+						pst.setInt(3, 1);
+						pst.execute();
+						pst.close();
+
+					} catch (Exception e1) {
+						JOptionPane.showMessageDialog(null, "be Patient !!!");
+					}
+
+				}
+
+			}
+		});
 
 	}
 
