a3615b694678aa739218e216095c09732f0ed502
OmarOdaini <ona11@msstate.edu>
2017-04-04 23:39:12 -0500

    try now
    fixed

--- src/view/BuyStockView.java
+++ src/view/BuyStockView.java
@@ -157,157 +157,7 @@ public class BuyStockView implements IView {
 			}
 		});
 		
-	/*	
-		btnSell.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-
-				Connection connection = dbconnection();
-				if (rdbtnMultiple.isSelected()) {
-					String Num_Pat = "^[0-9]{1,2}$"; // Pattern set for integers
-														// FIXME : fix it and
-														// make more than 1
-					String Input = NumOFStocks.getText();
-					utility.CommonUtil temp = new utility.CommonUtil();
-					if (temp.regexChecker(Num_Pat, Input)) {
-						int multiple = Integer.parseInt(Input); // convert to
-																// integer INPUT
-						// stop
-						try {
-
-							// get number of stocks of a specific company out of
-							// the DB
-							// Subtract and make sure the user have enough
-							// update the Number of stocks
-
-							String query = "SELECT COUNT(*) FROM OwnedStocks WHERE Price = ?"; // un--tested
-							PreparedStatement pst = connection.prepareStatement(query);
-							pst.setString(1, Price.getText());
-							int Number = pst.getUpdateCount();
-							System.out.println(Number);
-							ResultSet rs = pst.executeQuery();
-							int count = 0;
-							while (rs.next())
-								count++;
-							if (count >= multiple)
-								JOptionPane.showMessageDialog(null, "Selling Proccesed");
-							else
-								JOptionPane.showMessageDialog(null, "You Dont Have enough Stocks");
-
-							System.out.println(count);
-							rs.close();
-							pst.close();
-						} catch (Exception e1) {
-							JOptionPane.showMessageDialog(null, "Cant COUNT!!!");
-						}
-
-						// end of "if multiple is chosen."
-
-					} else
-						JOptionPane.showMessageDialog(null, "Please enter 2 to 99 for Number of Stocks"); // When
-																											// the
-																											// pattern
-																											// does
-																											// not
-																											// match
-
-				} else if (!rdbtnMultiple.isSelected()) {
-					// if multiple is not selected
-					// just subtract one from the NumOfStocks Column of the
-					// company specified
-					// OR delete the the row if one stock FOUND!
-					//
-					// Steps followed:
-					// 1 - type a selection query
-					// 2- Pull the number of the stocks for the company
-					// 3- Decide If you would update " more than one found CASE"
-					// or delete "just one found CASE"
-
-					//
-					//   
-
-					int NumOFStocks = 0;
-					try {
-						// This query is unTESTED
-						String query = "SELECT NumOfStocks FROM OwnedStocks WHERE Company = ? AND Price = ?"; // FIXME!!
-						PreparedStatement pst = connection.prepareStatement(query); // ERROR
-																					// because
-																					// the
-																					// SQL
-																					// not
-																					// Linked
-																					// to
-																					// this
-																					// class
-																					// yet
-						pst.setString(1, Company.getText());
-						pst.setString(2, Price.getText());
-
-						ResultSet rs = pst.executeQuery();
-						pst.execute();
-
-						NumOFStocks = rs.getInt("NumOfStocks");
-
-						int count = 0;
-						while (rs.next()) {
-
-							//  
-						}
-
-						rs.close();
-						pst.close();
-					} catch (Exception e1) {
-						JOptionPane.showMessageDialog(null, "Error !!!");
-					}
-
-					if (NumOFStocks > 1) { // This case means the user have more
-											// than one
-											// stock of the company so we would
-											// just decrement
-
-						try {
-
-							String query = "UPDATE OwnedStocks WHERE Company = ? SET NumOfStocks = NumOfStocks - 1";
-							PreparedStatement pst = connection.prepareStatement(query);
-							pst.setString(1, Company.getText());
-							pst.execute();
-							pst.close();
-
-						} catch (Exception e1) {
-							JOptionPane.showMessageDialog(null, "Cant Subtract one");
-						}
-
-					} else if (NumOFStocks == 1) { // This case means the user
-													// just have one stock so we
-													// would delete the row
-
-						try {
-
-							String query = "DELETE FROM OwnedStocks WHERE Company = ? AND BoughtPrice = ?AND ";
-							// first check if subtract
-							PreparedStatement pst = connection.prepareStatement(query);
-
-							pst.setString(1, Company.getText());
-							pst.setString(2, Price.getText());
-
-							pst.execute();
-							pst.close();
-
-						} catch (Exception e1) {
-
-							JOptionPane.showMessageDialog(null, "CANT DELETE !!!");
-						}
-
-					} else { // This case runs when the user us trying to sell
-								// stocks he d
-
-						JOptionPane.showMessageDialog(null, "Company not found !!!");
-					}
-
-				}
-
-			}
-		});
-*/
+	
 		// when cancel button pressed
 		btnCancel1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -324,7 +174,7 @@ public class BuyStockView implements IView {
 				Connection connection = dbconnection();
 				if (rdbtnMultiple.isSelected()) {
 					String Num_Pat = "^[0-9]{1,2}$"; // Reg exp pattern
-					String Input = NumOFStocks.getText(); // getting User input
+					String Input = NumText.getText(); // getting User input
 					int multiple = Integer.parseInt(Input); // change the input
 					utility.CommonUtil temp = new utility.CommonUtil();							// to int
 					if (temp.regexChecker(Num_Pat, Input)) { // check if the
@@ -346,13 +196,12 @@ public class BuyStockView implements IView {
 							pst.close();
 
 						} catch (Exception e1) {
-							JOptionPane.showMessageDialog(null, "be Patient !!!");
+							JOptionPane.showMessageDialog(null, "cant insert !!!");
 						}
 					} else
 						JOptionPane.showMessageDialog(null, "Input should be 2-99");
 
 				} else {
-					JOptionPane.showMessageDialog(null, "Thank god");
 					try {
 						String query = "INSERT INTO OwnedStocks (Company,BoughtPrice,NumOfStocks) VALUES (?,?,?)";
 						PreparedStatement pst = connection.prepareStatement(query);
